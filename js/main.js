ac22efb4f8cfd2106c4aa3607069a8d7818dfe8d
Yar Ffullex <72185613+Ffullex@users.noreply.github.com>
2021-01-28 12:27:57 +0700

    Add news area

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 $(function(){
 
-    $('.slider__inner').slick({
+    $('.slider__inner, .news__slider-inner').slick({
         nextArrow: '<button type="button" class="slick-btn slick-next"></button>',
         prevArrow: '<button type="button" class="slick-btn slick-prev"></button>',
         infinite: false,
@@ -9,6 +9,11 @@ $(function(){
     $('select').styler();
 
 
+/*    $('.news__slider-inner').slick({
+        nextArrow: '<button type="button" class="slick-btn slick-next"></button>',
+        prevArrow: '<button type="button" class="slick-btn slick-prev"></button>',
+        infinite: false,
+    }) */
 
 })
 
